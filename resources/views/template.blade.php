4af742bda204c79042228adf2d05473b506415be
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2022-09-07 14:00:11 +0100

    Alot 21

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -41,7 +41,7 @@
                 <li><a href="edit_settings/1" >Settings</a></li>
             @endif
         @endauth
-        <li><a href="help" >Help</a></li>
+        <li><a href="/help" >Help</a></li>
         <li>
 
 
