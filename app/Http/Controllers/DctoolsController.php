6449160e48c3cd501e211dfedd7eef2fe49638fc
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2023-07-05 16:44:48 +0100

    Modify Tool Generate Report3883

--- app/Http/Controllers/DctoolsController.php
+++ app/Http/Controllers/DctoolsController.php
@@ -334,7 +334,7 @@ class DctoolsController extends Controller
             $utilization = dctoolutilizations::whereRaw('? BETWEEN dated_from AND dated_to', [$from, $to])->get();
         }elseif($request->items[0]== "OVC"){
             $dctoolss = dctools::select('id')->where('category','OVC')->get()->toArray();
-            $utilization = dctoolutilizations::whereIn('item_id', $dctoolss)->whereRaw('? BETWEEN dated_from AND dated_to', [$from, $to])->get();
+            $utilization = dctoolutilizations::whereRaw('? BETWEEN dated_from AND dated_to', [$from, $to])->get();
         }elseif($request->items[0]== "Paediatrics"){
             $dctoolss = dctools::select('id')->where('category','Paediatrics')->get()->toArray();
             $utilization = dctoolutilizations::whereIn('item_id', $dctoolss)->whereRaw('? BETWEEN dated_from AND dated_to', [$from, $to])->get();
