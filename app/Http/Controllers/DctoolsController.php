fe8cd70ea840d0396d7556acde9872b80feea37a
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2023-08-17 12:15:43 +0100

    Fixed Confirmation sjsjs99

--- app/Http/Controllers/DctoolsController.php
+++ app/Http/Controllers/DctoolsController.php
@@ -377,10 +377,9 @@ class DctoolsController extends Controller
         if(Auth()->user()->role=="DCTManager"){
             $distribution = dcdistributions::where('sentto_state',Auth()->user()->state)->get();
 
-        }if(Auth()->user()->role=="DCTUser"){
+        }else if(Auth()->user()->role=="DCTUser"){
             $distribution = dcdistributions::where('sent_to',Auth()->user()->facility)->get();
-        }
-        else{
+        }else{
             $distribution = dcdistributions::all();
         }
         return view('confirm-dctools',compact('distribution'));
