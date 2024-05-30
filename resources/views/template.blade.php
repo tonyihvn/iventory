0eebf310723ad715814d6875995c34e08dcc71ff
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2024-05-30 13:31:21 +0100

    DCT Distribution Spreadsheet45

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -192,7 +192,10 @@
                                         <li><a class="waves-effect waves-green" href="{{ url('requests') }}"><i
                                                     class="material-icons">swap_horiz</i>Request for DCTools</a></li>
                                         <li><a class="waves-effect waves-blue" href="{{ url('/new-dctreport') }}"><i
-                                                    class="material-icons">swap_horiz</i>Generate DCT Report</a></li>
+                                                    class="material-icons">swap_horiz</i>Generate DCT Report</a>
+                                        <li><a class="waves-effect waves-blue" href="{{ url('/distribution-report') }}"><i
+                                            class="material-icons">swap_horiz</i>DCT Distribution Report</a></li>
+                                        </li>
                                                     @if(auth()->user()->role == 'DCTAdmin' || auth()->user()->role == 'DCTManager')
                                                     <li><a class="waves-effect waves-blue" href="{{ url('/register') }}"><i
                                                         class="material-icons">swap_horiz</i>Create Users</a></li>
