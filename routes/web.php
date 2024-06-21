aa65cd7bf5c9408f0f3f608490a1ae9d3a5e5694
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2024-06-21 13:11:18 +0100

    Addedddettt9898989848787687099nbjhjkl-8yhjh4983knd,dsdwkjlsd,sdsdwjsklkskwwjjkjjkk989898sdsdsd user dashboard 2

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Welcome and Home Pages
-Route::get('/', 'HomeController@index')->name('dashboard')->middleware('role:Admin,Manager,User,Observer,DCTAdmin,DCTManager,DCTUser');
+Route::get('/', 'HomeController@index')->name('dashboard')->middleware('role:Admin,Manager,Facility,User,Observer,DCTAdmin,DCTManager,DCTUser');
 
-Route::get('dashboard', 'HomeController@index')->name('dashboard')->middleware('role:Admin,Manager,User,Observer,DCTAdmin,DCTManager,DCTUser');
+Route::get('dashboard', 'HomeController@index')->name('dashboard')->middleware('role:Admin,Manager,Facility,User,Observer,DCTAdmin,DCTManager,DCTUser');
 
 Route::get('/home', 'HomeController@user_dashboard')->name('home')->middleware('auth');
 
